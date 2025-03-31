fb082fece065bb52063bd513d53fc421af62b749
janbildsoehansen <31623630+janbildsoehansen@users.noreply.github.com>
2025-03-31 08:58:23 +0200

    send-mail.php

--- send-mail.php
+++ send-mail.php
@@ -1,11 +1,11 @@
-<?php
+il<?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get the POST data
     $data = json_decode(file_get_contents('php://input'), true);
     $to = $data['to'];
     $subject = $data['subject'];
     $message = $data['message'];
-    $headers = "From: noreplay@xn--bildsetech-4cb.dk";
+    $headers = "From: noreplay@email.com";
 
     // Send the email
     if (mail($to, $subject, $message, $headers)) {
